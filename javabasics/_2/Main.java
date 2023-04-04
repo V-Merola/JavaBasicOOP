cd97b02d592c17e3f08d58d317eff377fdfe606f
vincenzo merola <vincenzo.merola18@gmail.com>
2023-04-04 17:31:37 +0200

    Risoluzione EX2

--- javabasics/_2/Main.java
+++ javabasics/_2/Main.java
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("\nExercise 2: ");
         System.out.print("Hello");
         //Write code underneath this
-
+        System.out.println(" Vincenzo!");
         //Write code above this
     }
 
@@ -36,9 +36,9 @@ public class Main {
     public static void exercise3() {
         System.out.println("\nExercise 3: ");
         // Change the code below here
-        System.out.println("This should ");
-        System.out.println("all appear ");
-        System.out.println("on the same line");
+        System.out.print("This should ");
+        System.out.print("all appear ");
+        System.out.print("on the same line");
         // and above here
     }
 }
